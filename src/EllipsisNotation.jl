326568e327e3edae3bd9270cae8e116e3b501aa2
antimon2 <antimon2.me@gmail.com>
2017-06-17 11:34:55 +0900

    Add more generic src and tests.

--- src/EllipsisNotation.jl
+++ src/EllipsisNotation.jl
@@ -3,16 +3,45 @@ __precompile__()
 module EllipsisNotation
 
   import Base.getindex, Base.setindex!
-  const ..   = Val{:..}
+  const .. = Val{:..}()
+  const IndexType = Union{(VERSION < v"0.6-" ? Real : Int), Val{:..}}
 
-  setindex!{T,N,N2}(A::AbstractArray{T,N}, x, ::Type{Val{:..}}, n::Vararg{Int,N2}) = A[(Colon() for i in 1:N-N2)..., n...] = x
-  getindex{T,N,N2}(A::AbstractArray{T,N}, ::Type{Val{:..}}, n::Vararg{Int,N2}) = A[(Colon() for i in 1:N-N2)..., n...]
+  setindex!{T,N,N2}(A::AbstractArray{T,N}, x, ::Val{:..}, n::Vararg{Int,N2}) = A[(Colon() for i in 1:N-N2)..., n...] = x
+  getindex{T,N,N2}(A::AbstractArray{T,N}, ::Val{:..}, n::Vararg{Int,N2}) = A[(Colon() for i in 1:N-N2)..., n...]
 
-  setindex!{T,N}(A::AbstractArray{T,N}, x, n::Int, ::Type{Val{:..}}) = A[n, (Colon() for i in 1:N-1)...] = x
-  #setindex!{T,N,N2}(A::AbstractArray{T,N}, x, n::Vararg{Int,N2}, ::Type{Val{:..}}) = A[n, (Colon() for i in 1:N-N2-1)...] = x
+  setindex!{T,N}(A::AbstractArray{T,N}, x, n::Int, ::Val{:..}) = A[n, (Colon() for i in 1:N-1)...] = x
+  
+  getindex{T,N}(A::AbstractArray{T,N}, n::Int, ::Val{:..}) = A[n,(Colon() for i in 1:N-1)...]
+  
+  function _parse_indextypes{N2}(N::Int, I::NTuple{N2,DataType})
+    dds = find(v -> v === Val{:..}, I)
+    return if length(dds) == 1
+      ddidx = dds[1]
+      ex = :(A[])
+      ex.args = Any[
+          ex.args;
+          Any[:(I[$i]) for i=1:ddidx-1];
+          Any[:(Colon()) for _=1:N-N2+1];
+          Any[:(I[$i]) for i=ddidx+1:N2]
+      ]
+      Nullable(ex)
+    else
+      Nullable{Expr}()
+    end
+  end
 
-  getindex{T,N}(A::AbstractArray{T,N}, n::Int, ::Type{Val{:..}}) = A[n,(Colon() for i in 1:N-1)...]
-  #getindex{T,N,N2}(A::AbstractArray{T,N}, n::Vararg{Int,N2}, ::Type{Val{:..}}) = A[n,(Colon() for i in 1:N-N2-1)...]
+  @generated function getindex{T,N,N2}(A::AbstractArray{T,N}, I::Vararg{IndexType,N2})
+    get(_parse_indextypes(N, I), :(throw(ArgumentError("`..` can only be specified in one place."))))
+  end
+
+  @generated function setindex!{T,N,N2}(A::AbstractArray{T,N}, x, I::Vararg{IndexType,N2})
+    ex_ = _parse_indextypes(N, I)
+    if !isnull(ex_)
+      :($(get(ex_)) = x)
+    else
+      :(throw(ArgumentError("`..` can only be specified in one place.")))
+    end
+  end
 
   export .., getindex, setindex!
 
