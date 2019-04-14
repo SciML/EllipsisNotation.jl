909b514f46abc54ef404fe9dbd039b8277361676
Lyndon White <oxinabox@ucc.asn.au>
2019-04-14 11:57:53 +0100

    Replace Val{..} with singleton Ellipsis type

--- src/EllipsisNotation.jl
+++ src/EllipsisNotation.jl
@@ -3,7 +3,9 @@ __precompile__()
 module EllipsisNotation
 
 import Base: to_indices, tail
-const ..   = Val{:..}()
+
+struct Ellipsis end
+const ..   = Ellipsis()
 
 @inline fillcolons(inds, I) = fillcolons((), inds, I)
 
@@ -12,7 +14,7 @@ const ..   = Val{:..}()
 @inline fillcolons(colons, t::NTuple{N, <:Any}, ::NTuple{N, <:Any}) where {N} = colons
 @inline fillcolons(colons, t::Tuple, s::Tuple) = fillcolons((colons..., :), tail(t), s)
 
-@inline function to_indices(A, inds, I::Tuple{Val{:..}, Vararg{Any, N}}) where N
+@inline function to_indices(A, inds, I::Tuple{Ellipsis, Vararg{Any, N}}) where N
     # Align the remaining indices to the tail of the `inds`
     colons = fillcolons(inds, tail(I))
     to_indices(A, inds, (colons..., tail(I)...))
@@ -20,7 +22,7 @@ end
 
 # avoid copying if indexing with .. alone, see
 # https://github.com/JuliaDiffEq/OrdinaryDiffEq.jl/issues/214
-@inline Base.getindex(A::AbstractArray, ::Val{:..}) = A
+@inline Base.getindex(A::AbstractArray, ::Ellipsis) = A
 
 export ..
 
