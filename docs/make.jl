8b21392afc46bcc579756401b2564ac8435b8b5d
ArnoStrouwen <arno.strouwen@telenet.be>
2022-10-25 23:16:40 +0200

    add formatting toml

--- docs/make.jl
+++ docs/make.jl
@@ -2,17 +2,13 @@ using Documenter, EllipsisNotation
 
 include("pages.jl")
 
-makedocs(
-    sitename = "EllipsisNotation.jl",
-    authors = "Chris Rackauckas",
-    modules = [EllipsisNotation],
-    clean = true,
-    doctest = false,
-    format = Documenter.HTML(
-        assets = ["assets/favicon.ico"],
-        canonical = "https://docs.sciml.ai/EllipsisNotation/stable/",
-    ),
-    pages = pages,
-)
+makedocs(sitename = "EllipsisNotation.jl",
+         authors = "Chris Rackauckas",
+         modules = [EllipsisNotation],
+         clean = true,
+         doctest = false,
+         format = Documenter.HTML(assets = ["assets/favicon.ico"],
+                                  canonical = "https://docs.sciml.ai/EllipsisNotation/stable/"),
+         pages = pages)
 
 deploydocs(repo = "github.com/SciML/EllipsisNotation.jl.git"; push_preview = true)
--- test/cartesian.jl
+++ test/cartesian.jl
@@ -6,5 +6,5 @@
 
     @test A[.., 1:1, 2:2, 1:3] == A[.., CartesianIndices((1:1, 2:2, 1:3))]
     @test A[1:1, .., 2:2, 1:3] == A[1:1, .., CartesianIndices((2:2, 1:3))]
-    @test A[1:2, 2, .., 1:3] == A[CartesianIndices((1:2,)), 2, .., CartesianIndices((1:3),)]
+    @test A[1:2, 2, .., 1:3] == A[CartesianIndices((1:2,)), 2, .., CartesianIndices((1:3))]
 end
