3621c62dbdffb657e43640bc990fd03b2ea55b05
ArnoStrouwen <arno.strouwen@telenet.be>
2023-06-22 20:50:31 +0200

    format

--- src/EllipsisNotation.jl
+++ src/EllipsisNotation.jl
@@ -21,38 +21,38 @@ possible confusion.
 # Example
 
 ```jldoctest
-julia> A = Array{Int}(undef,2,4,2);
+julia> A = Array{Int}(undef, 2, 4, 2);
 
-julia> A[..,1] = [2 1 4 5
-                  2 2 3 6];
+julia> A[.., 1] = [2 1 4 5
+           2 2 3 6];
 
-julia> A[..,2] = [3 2 6 5
-                  3 2 6 6];
+julia> A[.., 2] = [3 2 6 5
+           3 2 6 6];
 
-julia> A[:,:,1] == [2 1 4 5
-                    2 2 3 6]
+julia> A[:, :, 1] == [2 1 4 5
+           2 2 3 6]
 true
 
-julia> A[1,..] = reshape([3 4
-                          5 6
-                          4 5
-                          6 7],1,4,2) # drops singleton dimension
+julia> A[1, ..] = reshape([3 4
+               5 6
+               4 5
+               6 7], 1, 4, 2) # drops singleton dimension
 ...
 
 julia> B = [3 4
-            5 6
-            4 5
-            6 7];
+           5 6
+           4 5
+           6 7];
 
-julia> B == reshape(A[1,..],4,2)
+julia> B == reshape(A[1, ..], 4, 2)
 true
 ```
 """
 const .. = Ellipsis()
 
 @inline function to_indices(A,
-                            inds::NTuple{M, Any},
-                            I::Tuple{Ellipsis, Vararg{Any, N}}) where {M, N}
+    inds::NTuple{M, Any},
+    I::Tuple{Ellipsis, Vararg{Any, N}}) where {M, N}
     # Align the remaining indices to the tail of the `inds`
     colons = ntuple(n -> Colon(), M - _ndims_index(I) + 1)
     to_indices(A, inds, (colons..., tail(I)...))
