f5b377691387071ba1815cbe06b0ee5c2857dcbb
Arno Strouwen <arno.strouwen@telenet.be>
2024-02-22 15:13:37 +0100

    reapply formatting

--- test/basic.jl
+++ test/basic.jl
@@ -5,35 +5,35 @@ using StaticArrayInterface
 A = Array{Int}(undef, 2, 4, 2)
 
 A[.., 1] = [2 1 4 5
-    2 2 3 6]
+            2 2 3 6]
 
 A[.., 2] = [3 2 6 5
-    3 2 6 6]
+            3 2 6 6]
 
 @test A[:, :, 1] == [2 1 4 5
-    2 2 3 6]
+                     2 2 3 6]
 
 @test A[:, :, 2] == [3 2 6 5
-    3 2 6 6]
+                     3 2 6 6]
 
 @test A[:, .., 1] == [2 1 4 5
-    2 2 3 6]
+                      2 2 3 6]
 
 @test A[:, .., 1] == [2 1 4 5
-    2 2 3 6]
+                      2 2 3 6]
 
 A[1, ..] = reshape([3 4
-        5 6
-        4 5
-        6 7],
+                    5 6
+                    4 5
+                    6 7],
     1,
     4,
     2)
 
 B = [3 4
-    5 6
-    4 5
-    6 7]
+     5 6
+     4 5
+     6 7]
 
 @test B == reshape(A[1, ..], 4, 2) == reshape(view(A, 1, ..), 4, 2)
 
@@ -81,9 +81,9 @@ C[1, 1] += 1
     StaticArrayInterface.setindex!(A, reshape([3 4; 5 6; 4 5; 6 7], 1, 4, 2), 1, ..)
 
     B = [3 4
-        5 6
-        4 5
-        6 7]
+         5 6
+         4 5
+         6 7]
 
     @test B ==
           reshape(StaticArrayInterface.getindex(A, 1, ..), 4, 2) ==
